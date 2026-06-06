315df6348db98dfcac2423bf3895825457f36403
bacowan <cowan.brendan.a@gmail.com>
2026-06-06 20:29:05 +0900

    adding some error handling

--- scripts/crawl.ts
+++ scripts/crawl.ts
@@ -53,21 +53,18 @@ export default async function crawl(logTime: Date) {
     let page: { html: string, url: string } | null = null
     while (!page) {
         // Look through all the links of the latest page to select one to crawl.
-        const pageLinks = (await sql<{ url: string }[]>`
+        const latestPageLink = (await sql<{ url: string }[]>`
             select pl.url
             from crawl_history as ch
             inner join page_links as pl on ch.id = pl.parent_page
             where ch.bot_id = ${botId}
             and pl.is_crawling_okay = true
             and ch.log_time = (select max(log_time) from crawl_history where bot_id = ${botId})
-        `).map(r => r.url)
+            order by pl.initial_interest asc
+            limit 1
+        `)[0]
 
-        let interestingLink: string | null = null
-        if (pageLinks.length > 0) {
-            // from the links in the latest page, pick the most interesting one
-            // TODO: ask the llm to pick one
-            interestingLink = pageLinks[0]
-        }
+        let interestingLink: string | null = latestPageLink?.url ?? null
 
         if (interestingLink === null) {
             // there are no interesting links in the latest page. Pick one from the other pages
--- src/utils/robots.ts
+++ src/utils/robots.ts
@@ -3,12 +3,18 @@ import robotsParser from 'robots-parser'
 const cacheSeconds = Number(process.env.ROBOTS_TXT_CACHE_SECONDS) || 0
 
 export const isUrlCrawlable = async (url: string) => {
-    const { origin } = new URL(url)
-    const robotsUrl = `${origin}/robots.txt`
-    const res = await fetch(robotsUrl, {
-        next: { revalidate: cacheSeconds }
-    })
-    if (!res.ok) return true // no robots.txt means that crawling is okay
-    const robots = robotsParser(robotsUrl, await res.text())
-    return robots.isAllowed(url) ?? true
+    try {
+        const { origin } = new URL(url)
+        const robotsUrl = `${origin}/robots.txt`
+        const res = await fetch(robotsUrl, {
+            next: { revalidate: cacheSeconds }
+        })
+        if (!res.ok) return true // no robots.txt means that crawling is okay
+        const robots = robotsParser(robotsUrl, await res.text())
+        return robots.isAllowed(url) ?? true
+    }
+    catch {
+        // for  now we'll assume if there was an error then it was a problem with the page and should be skipped
+        return false
+    }
 }
